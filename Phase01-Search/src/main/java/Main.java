0a3bf91c9d1f533904d3cc37fa5a534f90814aae
Siasor88 <ssrahimi2@gmail.com>
2021-07-25 11:23:11 +0430

    close input stream in initilizing

--- Phase01-Search/src/main/java/Main.java
+++ Phase01-Search/src/main/java/Main.java
@@ -1,42 +1,41 @@
+import lombok.SneakyThrows;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.util.*;
 
 public class Main {
+    private static Scanner scanner;
+
     public static void main(String[] args) throws IOException, URISyntaxException {
-        closeInputStream();
-        Scanner scanner = new Scanner(System.in);
+        init();
+        run();
+    }
+
+    private static void run() {
         while (true) {
             String command = scanner.nextLine().trim();
             HashSet<Integer> searchResult = InvertedIndex.search(command);
             int count = 0;
-            for(int documentId : searchResult){
-                if (++ count > 5) break;
+            for (int documentId : searchResult) {
+                if (++count > 5) break;
                 Data data = new Data();
                 data.setIndexDocument(documentId);
                 System.out.println(data.getDocument().getContent());
-                System.out.println("######################################################################################################################################################################################");
+                System.out.println("#########################################################################################################################################################");
             }
         }
     }
 
-    private static void closeInputStream() {
-       InputStream stream = System.in;
-       System.setIn(new InputStream() {
-           @Override
-           public int read() throws IOException {
-               return 0;
-           }
-       });
-       InvertedIndex invertedIndex = new InvertedIndex();
-        TimerTask task = new TimerTask() {
-            @Override
-            public void run() {
-                System.setIn(stream);
-            }
-        };
-        Timer timer = new Timer();
-        timer.schedule(task,3000);
+    @SneakyThrows
+    private static void init() {
+        System.out.println("initializing database...");
+        InvertedIndex invertedIndex = new InvertedIndex();
+        System.out.println("initializing finished...");
+        scanner = new Scanner(System.in);
+        scanner = scanner.skip(".*");
+        scanner.next();
+        scanner = new Scanner(System.in);
     }
 }
