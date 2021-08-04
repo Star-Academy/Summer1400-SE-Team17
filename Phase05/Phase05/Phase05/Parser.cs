3f5380b2049fc6f5a7ce5826789338c91adeaf5f
Siasor88 <ssrahimi2@gmail.com>
2021-08-04 16:04:41 +0430

    ..

--- Phase05/Phase05/Phase05/Parser.cs
+++ Phase05/Phase05/Phase05/Parser.cs
@@ -1,67 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using edu.stanford.nlp.simple;
+using ikvm.extensions;
 using Phase05;
-using edu.stanford.nlp;
+
+using Document = edu.stanford.nlp.simple.Document;
+
+
 namespace Parser
 {
     public class WordParser : IParser<string>
     {
+        private PorterStemmer _stemmer = new PorterStemmer();
+
         public string Parse(string text)
         {
-           
-            throw new System.NotImplementedException();
+            
+            return _stemmer.StemWord(text);
         }
-        
+
     }
-    
+
     public class SentenceParser : IParser<string[]>
     {
         private IParser<string> _wordParse = new WordParser();
+
         public IParser<string> WordParser
         {
             set => _wordParse = value;
         }
+
         public string[] Parse(string text)
         {
-            throw new System.NotImplementedException();
+            text = text.toLowerCase();
+            text = text.replaceAll("[^a-z]", " ");
+            var words = text.Split(' ');
+            var result = new HashSet<string>();
+            foreach (var word in words)
+            {
+                result.Add(_wordParse.Parse(word));
+            }
+
+            return result.ToArray();
         }
     }
-    
+
     public class DocumentParser : IParser<string[]>
     {
         private IParser<string[]> _sentenceParser = new SentenceParser();
+
         public IParser<string[]> SentenceParser
         {
             set => _sentenceParser = value;
         }
+
         public string[] Parse(string text)
         {
-            throw new System.NotImplementedException();
+            var parsedResult = new HashSet<string>();
+            var sentences = text.Split('.');
+            foreach (var sentence in sentences)
+            {
+                parsedResult.UnionWith(_sentenceParser.Parse(sentence));
+            }
+
+            return parsedResult.ToArray();
         }
     }
 }
-
-
-
-
-//Package bandi??
-//Resource??
-// new Interface
-// exception handling
-//Export Method is Clean??
-
-//string a = Parse(a: 1, text: "11");
-
-//  public string Parse(string text,int a)
-// {
-//     throw new System.NotImplementedException();
-// }
-// public string Parse(int a,string text)
-// {
-//     throw new System.NotImplementedException();
-// }
-
-//yield return!?
-
-//wild card generic?
-
-//import java????
-//java awt????
