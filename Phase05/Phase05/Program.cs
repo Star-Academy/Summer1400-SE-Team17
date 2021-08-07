f5e3138f20504ea0c6cc2424deb902c860a843fa
Kalam132 <alipasha.montaseri@gmail.com>
2021-08-07 15:32:07 +0430

    modify project sdk

---
+++ Phase05/Phase05/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System;
+namespace Phase05
+{
+    internal class Program
+    {
+        private const string DataLocation = "EnglishData/";
+        public static void Main(string[] args)
+        {
+            SearchEngine searchEngine = new SearchEngine();
+            InvertedIndexSearcher invertedIndexSearcher = new InvertedIndexSearcher();
+            invertedIndexSearcher.LoadDictionary(DataLocation);
+            searchEngine.Searcher = invertedIndexSearcher;
+            string input = Console.ReadLine();
+            foreach (var document in searchEngine.Search(input))
+            {
+                Console.WriteLine($"{document.DocumentIndex} : {document.Content}");
+            }
+        }
+    }
+}
