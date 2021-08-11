1e17d8c501eee1f235ba61044fe117f170f073c8
Siasor88 <ssrahimi2@gmail.com>
2021-08-11 15:31:59 +0430

    make up this sh..

---
+++ Phase08/Phase08/Document.cs
@@ -0,0 +1,67 @@
+using System.Collections.Generic;
+
+namespace Phase05
+{
+    public class Document
+    {
+        public int DocumentId { get; set; }
+        public int DocumentIndex { get; set; }
+        public string Content { get; set; }
+        public IList<DocumentWord> DocumentWords { get; set; }
+
+        public Document(int documentIndex, string content)
+        {
+            DocumentIndex = documentIndex;
+            Content = content;
+            DocumentWords = new List<DocumentWord>();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Document) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return DocumentIndex;
+        }
+
+        protected bool Equals(Document other)
+        {
+            return DocumentIndex == other.DocumentIndex;
+        }
+    }
+
+    public class Word
+    {
+        public int WordId { get; set; }
+        public string Content { get; set; }
+        public IList<DocumentWord> DocumentWords { get; set; }
+
+        public Word(string content)
+        {
+            DocumentWords = new List<DocumentWord>();
+            Content = content;
+        }
+    }
+    public class DocumentWord
+    {
+        public int DocumentId { get; set; }
+        public Document Document { get; set; }
+        public int WordId { get; set; }
+        public Word Word { get; set; }
+
+        public DocumentWord(Document document, Word word)
+        {
+            Document = document;
+            Word = word;
+        }
+
+        public DocumentWord()
+        {
+        }
+    }
+}
